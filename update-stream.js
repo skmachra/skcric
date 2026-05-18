89506758573cac6bcc989bfaff1d5a893e674a45
Sunil <sunilmachra2021@gmail.com>
2026-05-18 19:38:30 +0530

    e

--- update-stream.js
+++ update-stream.js
@@ -1,24 +1,50 @@
 const fs = require("fs");
 
-const FILE = "Kayo.html";
+const file = process.env.FILE_NAME;
 
 const streamUrl = process.env.STREAM_URL;
+
 const clearkey = process.env.CLEARKEY;
 
 const [kid, key] = clearkey.split(":");
 
-let html = fs.readFileSync(FILE, "utf8");
+if (!fs.existsSync(file)) {
+    console.log("File not found");
+    process.exit(1);
+}
 
+let html = fs.readFileSync(file, "utf8");
+
+/* update stream url */
+html = html.replace(
+    /file:\s*"https?:\/\/.*?"/,
+    `file: "${streamUrl}"`
+);
+
+/* update shaka style stream url */
 html = html.replace(
     /const streamUrl = ".*?";/,
     `const streamUrl = "${streamUrl}";`
 );
 
+/* update keyId */
+html = html.replace(
+    /keyId:\s*"[a-f0-9]{32}"/,
+    `keyId: "${kid}"`
+);
+
+/* update key */
+html = html.replace(
+    /key:\s*"[a-f0-9]{32}"/,
+    `key: "${key}"`
+);
+
+/* update shaka clearkeys */
 html = html.replace(
     /"([a-f0-9]{32})"\s*:\s*"([a-f0-9]{32})"/,
     `"${kid}" : "${key}"`
 );
 
-fs.writeFileSync(FILE, html);
+fs.writeFileSync(file, html);
 
-console.log("Updated Kayo.html");
+console.log(`Updated ${file}`);
